ee8b5b4086cf579cc68f113ebc7f6a41704cd0f3
Egor Semenov <135758742+HinodeMyojo@users.noreply.github.com>
2024-06-18 23:27:48 +0300

    init entity

--- Program.cs
+++ Program.cs
@@ -1,16 +1,1 @@
-﻿using System.Xml;
-
-XmlDocument doc = new XmlDocument();
-doc.Load("/dev/Polygon/testfile.xml");
-XmlElement? root = doc.DocumentElement;
-if (root != null)
-{
-    foreach (XmlElement node in root)
-    {
-        Console.WriteLine(node.Name);
-        Console.WriteLine(node.InnerText);
-        Console.WriteLine(node.LocalName);
-        Console.WriteLine(node.ParentNode);
-        Console.WriteLine(node.Prefix);
-    }
-}
+﻿
